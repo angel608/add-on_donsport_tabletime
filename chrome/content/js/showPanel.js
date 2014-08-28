3438cb3f5980c749c1b15038242ee780196605b4
Salakhova Tatiana <salakhovat@gmail.com>
2014-08-28 11:53:54 +0400

    'second'

--- chrome/content/js/showPanel.js
+++ chrome/content/js/showPanel.js
@@ -1,19 +1,45 @@
 var showTimetable = {
 	winobj: null,
 	panel: 	null,
+	timehost: 	"http://time100.ru/",
 
 	init: function() {
     var obj = document.getElementById("navigator-toolbox");
     this.winobj = (obj)?window.document:window.opener.document;
-    this.panel = this.winobj.getElementById('frcPanel')
+    this.panel = this.winobj.getElementById('frcPanel');
 	},
 
 	createFrcPanel: function(parent) {
-		var now = new Date();
-		alert(now);
 		if (!this.panel)
-			this.init();
-		if (this.panel)
-			this.panel.openPopup(parent, 'after_start', 0, 0, false, true);
+				this.init();
+		if (this.panel) {    
+				this.setCurrentdate();
+				this.panel.openPopup(parent, 'after_start', 0, 0, false, true);
+			}
+	},
+
+	setCurrentdate: function() {
+		var today;
+		var req = new XMLHttpRequest();
+		req.open('GET', 'http://time100.ru/',true);
+		req.send(null);
+		var doc = document.implementation.createHTMLDocument();
+		doc.documentElement.innerHTML = req.responseText;
+		console.log(doc);
+		//showTimetable.editElementById("currentdate",today);
+	},
+
+	editElementById: function(id,value) {
+		var el = this.winobj.getElementById(id);
+		if (el) {
+			showTimetable.removeChilds(el);
+			var textNode = this.winobj.createTextNode(value);
+			el.appendChild(textNode);
+		}
+	},
+
+	removeChilds: function(obj) {
+			while (obj.firstChild) obj.removeChild(obj.firstChild);
 	}
+
 }; 

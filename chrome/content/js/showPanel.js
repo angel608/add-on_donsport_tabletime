b05dcd0deffb7ee16fca6fcd7e685ede3c58f00b
Salakhova Tatiana <salakhovat@gmail.com>
2014-09-04 13:56:49 +0400

    draft table 2

--- chrome/content/js/showPanel.js
+++ chrome/content/js/showPanel.js
@@ -1,43 +1,54 @@
 var showTimetable = {
 	winobj: null,
-	panel: 	null,
-	timehost: 	"http://24timezones.com/ru_vremia/moscow_mestnoe_vremia.php",
+	panel: null,
+	timehost: "http://24timezones.com/ru_vremia/moscow_mestnoe_vremia.php",
+	timetablehost: "http://www.donsport.ru/timetable/",
 
 	init: function() {
-    var obj = document.getElementById("navigator-toolbox");
-    this.winobj = (obj)?window.document:window.opener.document;
-    this.panel = this.winobj.getElementById('DonSportPanel');
+		var obj = document.getElementById("navigator-toolbox");
+		this.winobj = (obj) ? window.document : window.opener.document;
+		this.panel = this.winobj.getElementById('DonSportPanel');
 	},
 
 	createDonSportPanel: function(parent) {
 		if (!this.panel)
-				this.init();
-		if (this.panel) {    
-				this.setCurrentdate();
-				this.panel.openPopup(parent, 'after_start', 0, 0, false, true);
-			}
+			this.init();
+		if (this.panel) {
+			this.setCurrentdate();
+			this.getTimeTable();
+			this.panel.openPopup(parent, 'after_start', 0, 0, false, true);
+		}
 	},
 
 	setCurrentdate: function() {
-		var today;
 		var req = new XMLHttpRequest();
 		req.open('GET', showTimetable.timehost, false);
-		 req.onreadystatechange = function() { 
+		req.onreadystatechange = function() {
 			var doc = document.implementation.createHTMLDocument();
 			doc.documentElement.innerHTML = req.responseText;
-			today = doc.getElementById("currentTime");
-			 if (today!=null){
-			 	var month = (/\s(\D+) \d\d\d\d$/g).exec(today.innerHTML);
-			 	var date = (/ (\d\d),/).exec(today.innerHTML);
-			 	var dayofweek = (/\d\d:\d\d:\d\d, (\D+)\s/).exec(today.innerHTML);
-				showTimetable.editElementById("currentdate", showTimetable.uppercaseFirstLetter(month[1])+'	'+date[1]+',	'+dayofweek[1]);
-				}
-		 };
+			var today = doc.getElementById("currentTime");
+			if (today != null) {
+				var month = (/\s(\D+) \d\d\d\d$/g).exec(today.innerHTML);
+				var date = (/ (\d\d),/).exec(today.innerHTML);
+				var dayofweek = (/\d\d:\d\d:\d\d, (\D+)\s/).exec(today.innerHTML);
+				showTimetable.editElementById("currentdate", showTimetable.uppercaseFirstLetter(month[1]) + '	' + date[1] + ',	' + dayofweek[1]);
+			}
+		};
 
 		req.send(null);
 	},
 
-	editElementById: function(id,value) {
+	getTimeTable: function() {
+		var req = new XMLHttpRequest();
+		req.open('GET', showTimetable.timetablehost, false);
+		req.onreadystatechange = function() {
+			var doc = document.implementation.createHTMLDocument();
+			alert(req.responseText);
+		};
+		req.send(null);
+	},
+
+	editElementById: function(id, value) {
 		var el = this.winobj.getElementById(id);
 		if (el) {
 			showTimetable.removeChilds(el);
@@ -47,11 +58,11 @@ var showTimetable = {
 	},
 
 	removeChilds: function(obj) {
-			while (obj.firstChild) obj.removeChild(obj.firstChild);
+		while (obj.firstChild) obj.removeChild(obj.firstChild);
 	},
 
 	uppercaseFirstLetter: function(str) {
-		return str.replace(/^\D/,str.charAt(0).toUpperCase());
+		return str.replace(/^\D/, str.charAt(0).toUpperCase());
 	}
 
-}; 
+};
